cdcac0eaf971596435b3bcd7e78b17c689d1f253
mAlina258 <119889778+mAlina258@users.noreply.github.com>
2022-12-13 18:47:38 +0300

    Update FoxAnimal.py

--- FoxAnimal.py
+++ FoxAnimal.py
@@ -19,22 +19,4 @@ class FoxAnimal(BaseAnimal):
     def DoSound(self):
         print(self.name, "сказал/а на лисьем: ", self._sound)
 
-    def Eat(self, value, foodType):
-        if (foodType in self.__foodTypes and value<= self.__foodVolume):
-            self.__mass += value
-            self.__amountEaten += value
-            print("Лиса ", self.name, " говорит: покушал", value, foodType)
-        else:
-            print("Лиса ", self.name, " говорит: не буду", foodType)
-
-    def IsFeeded(self):
-        if (self.__amountEaten >= self.__foodVolume):
-            self.__isFeeded = True
-            print("Лиса ", self.name, " говорит: я наелся/ась!!")
-        else:
-            print(self.name, " говорит: я еще не наелся/ась")
-
-    def Play(self):
-        print("Лиса ", self.name, " говорит: поиграл!!")
-
 
