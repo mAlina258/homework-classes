0003f117395e6aecc4d949e0c1e2d558a5677622
mAlina258 <119889778+mAlina258@users.noreply.github.com>
2022-12-13 18:47:55 +0300

    Update ElephantAnimal.py

--- ElephantAnimal.py
+++ ElephantAnimal.py
@@ -1,22 +1,21 @@
 from BaseAnimal import *
 
-class FoxAnimal(BaseAnimal):
+class ElephantAnimal(BaseAnimal):
 
     def __init__(self, name):
-        super().__init__("Лиса", name)
-        self.__age = 4
-        self.__biome = "лес"
-        self._sound = "фр-фр-фр"
-        self.__liveSquare = 3  # размер клетки
-        self.__mass = 8
-        self.__isPredator = True  # хищник?
-        self.__foodTypes = ["ягоды", "мясо"]  # продукты
-        self.__foodVolume = 5  # кол-во еды
+        super().__init__("Слон", name)
+        self.__age = 20
+        self.__biome = "саванна"
+        self._sound = "*звуки слона*"
+        self.__liveSquare = 120  # размер клетки
+        self.__mass = 5000
+        self.__isPredator = False  # хищник?
+        self.__foodTypes = ["бананы", "сено"]  # продукты
+        self.__foodVolume = 50  # кол-во еды
         self.__amountEaten = 0  # кол-во съеденного
         self.__isFeeded = False  # сытость
 
 
     def DoSound(self):
-        print(self.name, "сказал/а на лисьем: ", self._sound)
-
+        print(self.name, "сказал/а на слоновьем: ", self._sound)
 
